d02f1fbad754e846e0ab37feeef9e59c28d56bab
JoseVS1 <105169283+JoseVS1@users.noreply.github.com>
2025-04-16 20:04:50 -0600

    Make created at date more readable

--- src/components/pages/ProfilePage.jsx
+++ src/components/pages/ProfilePage.jsx
@@ -1,9 +1,11 @@
 import { useContext } from "react"
 import UserContext from "../../context/UserContext"
 import { Link } from "react-router";
+import { parseISO, format } from "date-fns"
 
 export const ProfilePage = () => {
     const { user } = useContext(UserContext);
+    const formattedCreatedAt = format(parseISO(user.profile.createdAt), "MMMM do, yyyy");
 
   return (
     <div className="profile-page">
@@ -12,7 +14,7 @@ export const ProfilePage = () => {
                 <h2>Profile picture:</h2>
                 <img className="profile-picture" src={user.profile && user.profile.avatarUrl ? user.profile.avatarUrl : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"} alt="Profile picture" />
                 <h2>Display name: <span>{user.profile && user.profile.displayName ? user.profile.displayName : user.username}</span></h2>
-                <h2>Created at: <span>{user.profile && user.profile.createdAt}</span></h2>
+                <h2>Created at: <span>{formattedCreatedAt}</span></h2>
                 <h2>Bio:</h2>
                 <p>{user.profile && user.profile.bio ? user.profile.bio : "No bio provided."}</p>
                 
